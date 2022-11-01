db634cbfa2daaecfb4f7ec0e0ec254c317745f38
Erdal ALTIN <erdalaltin@gmail.com>
2022-11-01 17:45:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -251,7 +251,7 @@ if [ -x "$(command -v docker)" ] && [ -x "$(command -v docker-compose)" ]; then
 			done			
 			echo ""
 			echo "Reloading webserver ssl configuration"
-			docker exec webserver httpd -k restart > /dev/null 2>&1
+			docker container restart webserver > /dev/null 2>&1
 			echo "Ok."
 			echo ""
 			echo "completed setup"
